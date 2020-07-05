ccbff2bfd1cecb23e808764dfd1c4a0056a78b71
Ethan <ethan.johnson6oh@gmail.com>
2020-07-05 16:20:42 -0700

    do not make display events run on the gold cluster
    seems like this would waste a metric shitload of power
    
    Change-Id: Ia426788fc0e45037153f867ac1e8ea356bdc83da

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -4253,7 +4253,7 @@ case "$target" in
 	# cpuset parameters
 	echo 0-3 > /dev/cpuset/background/cpus
 	echo 0-3 > /dev/cpuset/system-background/cpus
-
+	echo 0-3 > /dev/cpuset/display/cpus
 	echo 0-6 > /dev/cpuset/foreground/cpus
 
 	# Turn off scheduler boost at the end
