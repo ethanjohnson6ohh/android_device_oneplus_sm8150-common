89c564751ecc979e89f2077a78e1d479a2746d9b
Ethan <ethan.johnson6oh@gmail.com>
2020-07-08 21:19:56 -0700

    cleanup
    Change-Id: Id4f3815f6919e933ffa80bd9b2a17d56e17f91f5

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -4313,9 +4313,6 @@ case "$target" in
         echo 0 > /sys/class/scsi_host/host0/../../../clkscale_enable
         #endif VENDOR_EDIT
 
-        echo 0-3 > /dev/cpuset/background/cpus
-        echo 0-3 > /dev/cpuset/system-background/cpus
-
         # Enable oom_reaper
 	if [ -f /sys/module/lowmemorykiller/parameters/oom_reaper ]; then
 		echo 1 > /sys/module/lowmemorykiller/parameters/oom_reaper
